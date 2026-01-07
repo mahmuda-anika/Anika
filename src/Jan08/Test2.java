2743961b1c06322aaad6dbbdcec50f4696df59b3
empowermentofwomen12345 <anikalutfunnahar@gmail.com>
2026-01-08 00:04:19 +0600

    Create Test2.java

---
+++ src/Jan08/Test2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jan08;
+
+/**
+ *
+ * @author USER
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("A");
+    }
+}
